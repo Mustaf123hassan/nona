0387ccc94e9f596d957c249f58ed31d127ba1f09
Mustaf123hassan <98228820+Mustaf123hassan@users.noreply.github.com>
2022-07-05 23:08:27 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -41,7 +41,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(
-                text="اشترك هنا", url="https://t.me/trtzb"
+                text="اشترك هنا", url="https://t.me/JFFFF"
             ),                        
         ],        
     ]
@@ -50,10 +50,10 @@ force_btn = InlineKeyboardMarkup(
 async def check_is_joined(message):    
     try:
         userid = message.from_user.id
-        status = await app.get_chat_member("trtzb", userid)
+        status = await app.get_chat_member("JFFFF", userid)
         return True
     except Exception:
-        await message.reply_text("عذرا ؏ُـمريـہ أنت غير مشترك في @trtzb ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        await message.reply_text("عذرا ؏ُـمريـہ أنت غير مشترك في @JFFFF ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
         return False
 
 # Command
